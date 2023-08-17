a5558d5ce19ca7b7d6dcf7a7717bf75026273552
Abastro <abab9579@gmail.com>
2023-08-17 14:17:07 +0900

    Tried unfold

--- n-step-hs/src/Chebyshev/Fraction.hs
+++ n-step-hs/src/Chebyshev/Fraction.hs
@@ -9,6 +9,7 @@ module Chebyshev.Fraction (
 import Chebyshev.Base
 import Control.Monad.State
 import Data.ExtendedReal
+import Data.List qualified as List
 import Data.Maybe
 import Data.MemoTrie
 import Data.Monoid (First (..))
@@ -16,6 +17,7 @@ import Data.Semigroup (Arg (..))
 import Data.Vector qualified as V
 import Inductive
 import Streamly.Data.Fold qualified as Fold
+import Streamly.Data.Unfold qualified as Unfold
 import Streamly.Prelude qualified as Stream
 import Util
 
@@ -34,8 +36,8 @@ chebyRealFraction u2 s_k1 = case previous s_k1 of
 -- | Initiate fraction computation of chebyshev polynomials.
 initChebyRealFrac :: (RealFrac v, Integral a) => v -> InductiveEval a (Extended v)
 initChebyRealFrac u2 = inductive induction 0
-  where
-    induction n_k g_k = 1 `infiDiv` knownFinite (Finite u2 * (fromIntegral n_k - value g_k))
+ where
+  induction n_k g_k = 1 `infiDiv` knownFinite (Finite u2 * (fromIntegral n_k - value g_k))
 
 -- >>> chebyRealFractionMax 3 3
 -- Arg (Finite (2 % 3)) [1,1,1]
@@ -45,6 +47,8 @@ initChebyRealFrac u2 = inductive induction 0
 
 -- >>> chebyRealFractionMax (8/3) 5
 -- Arg PosInf [6,1,1,1,1]
+
+type FractionEval = InductiveEval Integer (Extended Rational)
 
 -- | Compute maximal real-fraction G_k given k and steps.
 chebyRealFractionMax :: Rational -> Word -> Arg (Extended Rational) (V.Vector Integer)
@@ -97,39 +101,43 @@ chebyRealFractionMax u2 = computeMax
       maxCandArg = maxCandidate k
 
       chooseN_i ::
-        InductiveEval Integer (Extended Rational) ->
         Word ->
-        Stream.SerialT (State (V.Vector Rational)) (InductiveEval Integer (Extended Rational))
-      chooseN_i g_L i = do
-        let vG_L = knownFinite $ value g_L
-        boundRadius <- Stream.fromEffect $ gets (V.! (fromIntegral i - 1))
-        -- when (i <= 3) $ get >>= \curMax -> traceShowM (k, i, inputs s_L, curMax, g_L, boundRadius)
-        let centerBnd = floor vG_L
-            minBnd = floor $ vG_L - boundRadius
-            maxBnd = ceiling $ vG_L + boundRadius
-        n_i <-
-          if i == 1
-            then Stream.enumerateFromTo (max 1 minBnd) maxBnd -- Sign, take n_1 > 0
-            else Stream.filter (/= 0) $ streamRangeFromCenter centerBnd minBnd maxBnd
-        pure (next n_i g_L)
+        Unfold.Unfold (State (V.Vector Rational)) FractionEval FractionEval
+      chooseN_i i =
+        flip Unfold.lmapM Unfold.fromStream $ \g_L -> do
+          boundRadius <- gets (V.! (fromIntegral i - 1))
+          let vG_L = knownFinite $ value g_L
+              center = floor vG_L
+              minBnd = floor $ vG_L - boundRadius
+              maxBnd = ceiling $ vG_L + boundRadius
+          pure $ (`next` g_L) <$> do
+            if i == 1
+              then Stream.enumerateFromTo (max 1 minBnd) maxBnd -- Only take n_1 > 0
+              else Stream.filter (/= 0) $ rangeFromCenter center minBnd maxBnd
 
       withArgPut argValue@(Arg val _) = argValue <$ put (boundRadiusVec k val)
+      -- Puts max to state and return.
       puttingMax oldMax new = if oldMax < new then withArgPut new else pure oldMax
 
+      inductAsArg g_k = Arg (abs <$> value g_k) (V.fromList $ inputs g_k)
+
       maxFinding :: Stream.SerialT (State (V.Vector Rational)) (Arg (Extended Rational) (V.Vector Integer))
-      maxFinding = Stream.scanlM' puttingMax (withArgPut maxCandArg) $ do
-        g_k <- foldM chooseN_i (initChebyRealFrac u2) [1 .. k]
-        pure $ Arg (abs <$> value g_k) (V.fromList $ inputs g_k)
+      maxFinding =
+        Stream.scanlM' puttingMax (withArgPut maxCandArg) $ inductAsArg <$> do
+          List.foldl'
+            (\stream i -> Stream.unfoldMany (chooseN_i i) stream)
+            (Stream.fromPure $ initChebyRealFrac u2)
+            [1 .. k]
 
 -- Stops when infinity is encountered
 untilInfinity :: (Monad m) => Fold.Fold m (Arg (Extended r) b) (Maybe (Arg (Extended r) b))
 untilInfinity = Fold.takeEndBy (\(Arg curMax _) -> Data.ExtendedReal.isInfinite curMax) Fold.last
 
--- >>> Stream.toList $ streamRangeFromCenter (0 :: Int) (-3) 6
+-- >>> Stream.toList $ rangeFromCenter (0 :: Int) (-3) 6
 -- [0,1,-1,2,-2,3,-3,4,5,6]
 
-streamRangeFromCenter :: (Enum a, Stream.Enumerable a, Monad m) => a -> a -> a -> Stream.SerialT m a
-streamRangeFromCenter center lower higher =
+rangeFromCenter :: (Enum a, Stream.Enumerable a, Monad m) => a -> a -> a -> Stream.SerialT m a
+rangeFromCenter center lower higher =
   Stream.enumerateFromThenTo center (pred center) lower
     `Stream.wSerial` Stream.enumerateFromTo (succ center) higher
 
