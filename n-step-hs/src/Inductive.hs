bb57c83d60d333c9d971dc5ba0680f7e3ff96f5f
Abastro <abab9579@gmail.com>
2023-08-24 16:24:53 +0900

    Use Seq in inductive

--- n-step-hs/src/Inductive.hs
+++ n-step-hs/src/Inductive.hs
@@ -10,6 +10,7 @@ module Inductive (
 ) where
 
 import Data.List
+import Data.Sequence qualified as Seq
 
 data InductiveResult a v
   = Initial !v
@@ -19,7 +20,8 @@ data InductiveResult a v
 -- | Datatype for an inductive evaluation of [a] -> v.
 data InductiveEval a v = InductiveEval
   { induction :: a -> InductiveEval a v -> v,
-    evaluated :: InductiveResult a v
+    initial :: v,
+    evaluated :: Seq.Seq (a, v)
   }
 
 instance (Show a, Show v) => Show (InductiveEval a v) where
@@ -27,32 +29,30 @@ instance (Show a, Show v) => Show (InductiveEval a v) where
   show eval = show eval.evaluated
 
 inductive :: (a -> InductiveEval a v -> v) -> v -> InductiveEval a v
-inductive induction initValue = InductiveEval{induction, evaluated = Initial initValue}
+inductive induction initValue = InductiveEval{induction, initial = initValue, evaluated = Seq.empty}
 
 next :: a -> InductiveEval a v -> InductiveEval a v
 next inp prev =
-  prev
-    { evaluated = Next inp (prev.induction inp prev) prev.evaluated
-    }
+  prev{evaluated = prev.evaluated Seq.|> (inp, prev.induction inp prev)}
 
 -- Consecutive calls to 'next'
 nexts :: [a] -> InductiveEval a v -> InductiveEval a v
-nexts inps prev = foldl (flip next) prev inps
+nexts inps prev = foldl' (flip next) prev inps
 
 previous :: InductiveEval a v -> Maybe (a, InductiveEval a v)
 previous cur = case cur.evaluated of
-  Initial _ -> Nothing
-  Next inp _ prev -> Just (inp, cur{evaluated = prev})
+  Seq.Empty -> Nothing
+  prev Seq.:|> (inp, _) -> Just (inp, cur{evaluated = prev})
 
 value :: InductiveEval a v -> v
 value eval = case eval.evaluated of
-  Initial v -> v
-  Next _ v _ -> v
+  Seq.Empty -> eval.initial
+  _ Seq.:|> (_, v) -> v
 
 input :: InductiveEval a v -> Maybe a
 input eval = case eval.evaluated of
-  Initial _ -> Nothing
-  Next inp _ _ -> Just inp
+  Seq.Empty -> Nothing
+  _ Seq.:|> (inp, _) -> Just inp
 
 -- |
 -- > inputs (nexts inps (inductive ind v0)) == inps
